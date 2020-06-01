d9e79f374b597434f04c0909e3b419da77a68500
Somayeh Yarahmadi <somayeh.yarahmadi@gmail.com>
2020-06-01 19:22:19 -0400

    Remove get_filters function and add main function

--- bikeshare.py
+++ bikeshare.py
@@ -7,40 +7,6 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-def get_filters():
-      print('Hello! Let\'s explore some US bikeshare data!')
-      print('Would you like to see data for Chicago, New York, or Washington?')
-      city=input().lower()
-      while city not in ['chicago','new york city','washington']:
-        print('Entered city is invalid, try again')
-        city=input().lower()
-      print('Awesome, you chose {}'.format(city))
-      df = pd.read_csv(CITY_DATA[city])
-      print('Would you like to filter the data by month, day, both or not at all?\n (Please enter month, day, both or none)')
-      word=input().lower()
-      while word not in ['month', 'day', 'both','none']:
-        print('Please enter month, day, both or none')
-        word=input().lower()
-      if word in ['month', 'both']:
-        print('Which month - January, February, March, April, May, or June?')
-        month=input().lower()
-        while month not in ['january', 'february', 'march', 'april', 'may', 'june']:
-          print('please enter January, February, March, April, May, or June')
-          month=input().lower()
-      else:
-        month='all'
-      if word in ['day', 'both']:
-        print('Which day - Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, or Sunday? \n please give it as integer, Sunday=1')
-        #print('Which day - Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, or Sunday?')
-        day=int(input())
-        while day not in [1,2,3,4,5,6,7]:
-        #while day not in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']:
-          #print('please enter Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, or Sunday.')
-          print('please enter Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, or Sunday.\n please give it as integer, Sunday=1')
-          day=int(input())
-      else:
-        day='all'
-      return city, month, day
 
       #---------------------------------
 
@@ -165,3 +131,27 @@ def user_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 #------------------------------------------------
+
+def main():
+    while True:
+        city, month, day = get_filters()
+        df = load_data(city, month, day)
+
+        time_stats(df)
+        station_stats(df)
+        trip_duration_stats(df)
+        user_stats(df)
+        raw_data= input('\nWould you see 5 raws of data? Enter yes or no.\n')
+        start=0
+        end=5
+        while raw_data.lower()=='yes':
+          print(df.iloc[start:end])
+          start+=5
+          end+=5
+          raw_data= input('\nWould you see 5 more raws of data? Enter yes or no.\n')
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
+#-----------------------------------------------
+if __name__ == "__main__":
+	main()
